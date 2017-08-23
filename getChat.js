c1a6a2b31a2e668dd04c04af6738f4b1e93d4fe7
Christian Moen <cmoen11@gmail.com>
2017-08-23 22:57:05 +0200

    Måtte endre måten jeg sendte data til galtvortskolen, da den ikke fulgte riktig charset.

--- getChat.js
+++ getChat.js
@@ -14,7 +14,7 @@ $(function(){
                     $('#fullscrape').html('cleard');
                 },
             });
-        },100000); // 5000ms == 5 seconds
+        },1000); // 5000ms == 5 seconds
 });
 
 $('#chat_input_field').bind('keyup', function(e) {
@@ -28,16 +28,63 @@ $('#chat_input_field').bind('keyup', function(e) {
 });
 
 function postMsg(msg) {
-    var data = {chat_message:msg.trim(), "chat_submit": "Send"}
+    msg = msg.replace(/\n|\r/g, "");
+    msg = escape(msg).replace(/\+/g, "%2B");
+    var data = "chat_message="+msg.trim()+ "&chat_submit=Send";
     console.log(msg);
     $.ajax({
         type:"POST",
         url: "http://galtvortskolen.net/",
+        processData: false,
         data: data,
-        contentType: 'Content-type: text/plain; charset=iso-8859-1',
-        beforeSend: function(jqXHR) {
-            jqXHR.overrideMimeType('text/html;charset=iso-8859-1');
+        success: function (data) {
+            console.log(data);
         },
-        success: success
+        dataType: "html",
+        contentType: "application/x-www-form-urlencoded"
     })
 }
+function insertAtCaret(areaId, text) {
+    var txtarea = document.getElementById(areaId);
+    if (!txtarea) { return; }
+
+    var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
+        "ff" : (document.selection ? "ie" : false ) );
+    if (br == "ie") {
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        strPos = range.text.length;
+    } else if (br == "ff") {
+        strPos = txtarea.selectionStart;
+    }
+
+    var front = (txtarea.value).substring(0, strPos);
+    var back = (txtarea.value).substring(strPos, txtarea.value.length);
+    txtarea.value = front + text + back;
+    strPos = strPos + text.length;
+    if (br == "ie") {
+        txtarea.focus();
+        var ieRange = document.selection.createRange();
+        ieRange.moveStart ('character', -txtarea.value.length);
+        ieRange.moveStart ('character', strPos);
+        ieRange.moveEnd ('character', 0);
+        ieRange.select();
+    } else if (br == "ff") {
+        txtarea.selectionStart = strPos;
+        txtarea.selectionEnd = strPos;
+        txtarea.focus();
+    }
+
+    txtarea.scrollTop = scrollPos;
+}
+
+$('#quote_start').click(function () {
+    $('textarea#textareaid').append('«');
+});
+
+$('#quote_end').click(function () {
+    $('textarea#textareaid').append('»');
+});
